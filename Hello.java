4fb8b046ef498b1153dbeb7ecd1827d9ae837132
Chuks <saghulor@gmail.com>
2024-08-20 08:36:53 +0100

    this is my Hello.java commit file

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+       // prints "hello, world" in the terminal window.
+        System.out.println("Hello, World!");
+    }
+}
